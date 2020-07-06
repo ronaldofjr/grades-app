032b3a2cf1d787b6dcb5a732a6724e4c1fc08a36
ronaldofjr <ronaldofjr@gmail.com>
2020-07-06 14:08:57 -0300

    'ADIC'

--- src/http-common.js
+++ src/http-common.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
-  headers: {
-    'Content-type': 'application/json',
-  },
+	baseURL: 'https://grades-app.herokuapp.com/',
+	headers: {
+		'Content-type': 'application/json',
+	},
 });
